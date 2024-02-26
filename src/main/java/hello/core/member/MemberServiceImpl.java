8c34ac14dba774ff650a7d42ca061076fc60351d
ChangMinPark2 <98qkrckdals@naver.com>
2024-02-26 10:30:19 +0900

    feat(Spring-core): 회원 도메인

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,8 +1,8 @@
 package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
-
     private final MemberRepository memberRepository = new MemoryMemberRepository();
+    // 인터페이스, 구현체 둘다 의존. DIP 위반
     @Override
     public void join(Member member) {
         memberRepository.save(member);
